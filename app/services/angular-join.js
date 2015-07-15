da64236cc0ddf1c2dd32b6313ebfeff7198fa978
Ali Tavakoli <ali.tavakoli@gmail.com>
2015-07-15 12:16:28 -0400

    Added duplicate row support for mergeJoin
    The mergeJoin algorithm now does a cross/cartesian join when joining duplicate
    (with respect to the comparator) rows. This is how hashJoin already works.
    
    e.g.
    
        Join.mergeJoin(
          [ {x: 1, y: 1},
            {x: 1, y: 2},
            {x: 2, y: 3},
            {x: 3, y: 4}
          ],
          [ {x: 1, y: 5},
            {x: 1, y: 6},
            {x: 2, y: 7},
            {x: 4, y: 8}
          ],
          function(l, r) {
            return l.x - r.x;
          },
          function(l, r) {
            if (l && r) {
              return {x: l.x, y1: l.y, y2: r.y};
            } else if (l) {
              return {x: l.x, y1: l.x, y2: 0};
            } else {
              return {x:r.x, y1: 0, y2: r.x};
            }
          }
        );
    
    Used to return:
    
        [
          {x: 1, y1: 1, y2: 5},
          {x: 1, y1: 2, y2: 6},
    
          {x: 2, y1: 3, y2: 7},
          {x: 3, y1: 4, y2: 0},
          {x: 4, y1: 0, y2: 8}
        ]
    
    But now returns:
    
        [
          {x: 1, y1: 1, y2: 5},
          {x: 1, y1: 1, y2: 6},
          {x: 1, y1: 2, y2: 5},
          {x: 1, y1: 2, y2: 6},
    
          {x: 2, y1: 3, y2: 7},
          {x: 3, y1: 4, y2: 0},
          {x: 4, y1: 0, y2: 8}
        ]

--- app/services/angular-join.js
+++ app/services/angular-join.js
@@ -5,34 +5,80 @@ angular.module('angular-join', [])
 
 .factory('Join', function() {
   function mergeJoin(a1, a2, comparator, callback, options) {
-    var i1 = 0, i2 = 0;
     var a3 = [];
   
     if (options && options.sort) {
       a1.sort(comparator);
       a2.sort(comparator);
     }
-    
+
+    function getContiguousLength(src, start) {
+      if (start < src.length) {
+        var i = start + 1;
+        while (i < src.length && comparator(src[start], src[i]) === 0) {
+          i++;
+        }
+        return i - start;
+      } else {
+        return 0;
+      }
+    }
+
+    function addLeft(start, end) {
+      for (var i = start; i < end; ++i) {
+        var newElement = callback(a1[i], null);
+        if (newElement) {
+          a3.push(newElement);
+        }
+      }
+    }
+
+    function addRight(start, end) {
+      for (var i = start; i < end; ++i) {
+        var newElement = callback(null, a2[i]);
+        if (newElement) {
+          a3.push(newElement);
+        }
+      }
+    }
+
+    function addCartesianJoin(start1, end1, start2, end2) {
+      for (var i1 = start1; i1 < end1; ++i1) {
+        for (var i2 = start2; i2 < end2; ++i2) {
+          var newElement = callback(a1[i1], a2[i2]);
+          if (newElement) {
+            a3.push(newElement);
+          }
+        }
+      }
+    }
+
+    var i1 = 0, i2 = 0;
+    var len1 = getContiguousLength(a1, i1);
+    var len2 = getContiguousLength(a2, i2);
+
     while (i1 < a1.length && i2 < a2.length) {
       var compare = comparator(a1[i1], a2[i2]);
-      var newElement;
       if (compare === 0) {
-        newElement = callback(a1[i1++], a2[i2++]);
+        addCartesianJoin(i1, i1 + len1, i2, i2 + len2);
+        i1 += len1;
+        i2 += len2;
+        len1 = getContiguousLength(a1, i1);
+        len2 = getContiguousLength(a2, i2);
       } else if (compare < 0) {
-        newElement = callback(a1[i1++], null);
+        addLeft(i1, i1 + len1);
+        i1 += len1;
+        len1 = getContiguousLength(a1, i1);
       } else {
-        newElement = callback(null, a2[i2++]);
+        addRight(i2, i2 + len2);
+        i2 += len2;
+        len2 = getContiguousLength(a2, i2);
       }
-      if (newElement) {
-        a3.push(newElement);
-      }
     }
-    while (i1 < a1.length) {
-      a3.push(callback(a1[i1++], null));
-    }
-    while (i2 < a2.length) {
-      a3.push(callback(null, a2[i2++]));
-    }
+
+    addLeft(i1, a1.length);
+    addRight(i2, a2.length);
+
     return a3;
   }
   
