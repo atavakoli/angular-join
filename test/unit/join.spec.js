3caa7c6a9d0fd305dbf1835afb5d59db6dc01381
Ali Tavakoli <ali.tavakoli@gmail.com>
2015-08-13 18:39:31 -0400

    Streamlined join testspec
    Since many of the tests were just minorly different iterations of the same
    basic test, the test parameters were grouped together and tests are now
    generated via loops. New test-cases can be added by providing an object with
    the parameters that'd get passed into a join operation (left/right arrays,
    hash/comparator functions, field to use for string version of hash/comparator,
    the join callback function, and finally, the expected result array).

--- test/unit/join.spec.js
+++ test/unit/join.spec.js
@@ -20,105 +20,129 @@ describe('join', function() {
     { x: 5, y: 8 }
   ];
 
-  function innerJoin(e1, e2) {
-    if (e1 && e2) {
-      return {
-        x:  e1.x,
-        y1: e1.y,
-        y2: e2.y
-      };
-    } else {
-      return null;
-    }
+  function hashFcn(e) {
+    return e.x;
   }
 
-  var innerExpected = [
-    { x: 1, y1: 1, y2: 5 },
-    { x: 2, y1: 2, y2: 6 },
-    { x: 3, y1: 3, y2: 7 }
-  ];
-
-  function leftJoin(e1, e2) {
-    if (e1 && e2) {
-      return {
-        x:  e1.x,
-        y1: e1.y,
-        y2: e2.y
-      };
-    } else if (e1) {
-      return {
-        x: e1.x,
-        y1: e1.y,
-        y2: null
-      };
-    } else {
-      return null;
-    }
+  function comparator(e1, e2) {
+    return e1.x - e2.x;
   }
 
-  var leftExpected = [
-    { x: 1, y1: 1, y2: 5 },
-    { x: 2, y1: 2, y2: 6 },
-    { x: 3, y1: 3, y2: 7 },
-    { x: 4, y1: 4, y2: null }
-  ];
-
-  function rightJoin(e1, e2) {
-    if (e1 && e2) {
-      return {
-        x:  e1.x,
-        y1: e1.y,
-        y2: e2.y
-      };
-    } else if (e2) {
-      return {
-        x: e2.x,
-        y1: null,
-        y2: e2.y
-      };
-    } else {
-      return null;
+  var tests = [
+    {
+      name: 'inner',
+      left: left, right: right,
+      hash: hashFcn, comparator: comparator, field: 'x',
+      join: function(e1, e2) {
+        if (e1 && e2) {
+          return {
+            x:  e1.x,
+            y1: e1.y,
+            y2: e2.y
+          };
+        } else {
+          return null;
+        }
+      },
+      expected: [
+        { x: 1, y1: 1, y2: 5 },
+        { x: 2, y1: 2, y2: 6 },
+        { x: 3, y1: 3, y2: 7 }
+      ]
+    },
+    {
+      name: 'left',
+      left: left, right: right,
+      hash: hashFcn, comparator: comparator, field: 'x',
+      join: function(e1, e2) {
+        if (e1 && e2) {
+          return {
+            x:  e1.x,
+            y1: e1.y,
+            y2: e2.y
+          };
+        } else if (e1) {
+          return {
+            x: e1.x,
+            y1: e1.y,
+            y2: null
+          };
+        } else {
+          return null;
+        }
+      },
+      expected: [
+        { x: 1, y1: 1, y2: 5 },
+        { x: 2, y1: 2, y2: 6 },
+        { x: 3, y1: 3, y2: 7 },
+        { x: 4, y1: 4, y2: null }
+      ]
+    },
+    {
+      name: 'right',
+      left: left, right: right,
+      hash: hashFcn, comparator: comparator, field: 'x',
+      join: function(e1, e2) {
+        if (e1 && e2) {
+          return {
+            x:  e1.x,
+            y1: e1.y,
+            y2: e2.y
+          };
+        } else if (e2) {
+          return {
+            x: e2.x,
+            y1: null,
+            y2: e2.y
+          };
+        } else {
+          return null;
+        }
+      },
+      expected: [
+        { x: 1, y1: 1,    y2: 5 },
+        { x: 2, y1: 2,    y2: 6 },
+        { x: 3, y1: 3,    y2: 7 },
+        { x: 5, y1: null, y2: 8 }
+      ]
+    },
+    {
+      name: 'full',
+      left: left, right: right,
+      hash: hashFcn, comparator: comparator, field: 'x',
+      join: function(e1, e2) {
+        if (e1 && e2) {
+          return {
+            x:  e1.x,
+            y1: e1.y,
+            y2: e2.y
+          };
+        } else if (e1) {
+          return {
+            x: e1.x,
+            y1: e1.y,
+            y2: null
+          };
+        } else {
+          return {
+            x: e2.x,
+            y1: null,
+            y2: e2.y
+          };
+        }
+      },
+      expected: [
+        { x: 1, y1: 1,    y2: 5 },
+        { x: 2, y1: 2,    y2: 6 },
+        { x: 3, y1: 3,    y2: 7 },
+        { x: 4, y1: 4,    y2: null },
+        { x: 5, y1: null, y2: 8 }
+      ]
     }
-  }
 
-  var rightExpected = [
-    { x: 1, y1: 1,    y2: 5 },
-    { x: 2, y1: 2,    y2: 6 },
-    { x: 3, y1: 3,    y2: 7 },
-    { x: 5, y1: null, y2: 8 }
   ];
 
-  function fullJoin(e1, e2) {
-    if (e1 && e2) {
-      return {
-        x:  e1.x,
-        y1: e1.y,
-        y2: e2.y
-      };
-    } else if (e1) {
-      return {
-        x: e1.x,
-        y1: e1.y,
-        y2: null
-      };
-    } else {
-      return {
-        x: e2.x,
-        y1: null,
-        y2: e2.y
-      };
-    }
-  }
-
-  var fullExpected = [
-    { x: 1, y1: 1,    y2: 5 },
-    { x: 2, y1: 2,    y2: 6 },
-    { x: 3, y1: 3,    y2: 7 },
-    { x: 4, y1: 4,    y2: null },
-    { x: 5, y1: null, y2: 8 }
-  ];
-
-  describe('using a hash function', function() {
+  describe('using a hash-join algorithm', function() {
     var permutationOf = function(a2) {
       return {
         asymmetricMatch: function(a1) {
@@ -141,244 +165,119 @@ describe('join', function() {
       }
     }
 
-    function hashFcn(e) {
-      return e.x;
-    }
+    describe('called statically', function() {
+      tests.forEach(function(t) {
+        describe('should do ' + t.name + '-join', function() {
+          it('with a function', function() {
+            var result = Join.hashJoin(t.left, t.right, t.hash, t.join);
+            expect(result).toEqual(permutationOf(t.expected));
+          });
 
-    describe('using the static method', function() {
-      it('should do inner join with a function', function() {
-        var result = Join.hashJoin(left, right, hashFcn, innerJoin);
-        expect(result).toEqual(permutationOf(innerExpected));
+          it('with a string', function() {
+            var result = Join.hashJoin(t.left, t.right, t.field, t.join);
+            expect(result).toEqual(permutationOf(t.expected));
+          });
+
+          it('with an array', function() {
+            var result = Join.hashJoin(t.left, t.right, [t.field], t.join);
+            expect(result).toEqual(permutationOf(t.expected));
+          });
+        });
       });
 
-      it('should do inner join with a string', function() {
-        var result = Join.hashJoin(left, right, 'x', innerJoin);
-        expect(result).toEqual(permutationOf(innerExpected));
-      });
-
-      it('should do left join with a function', function() {
-        var result = Join.hashJoin(left, right, hashFcn, leftJoin);
-        expect(result).toEqual(permutationOf(leftExpected));
-      });
-
-      it('should do left join with a string', function() {
-        var result = Join.hashJoin(left, right, 'x', leftJoin);
-        expect(result).toEqual(permutationOf(leftExpected));
-      });
-
-      it('should do right join with a function', function() {
-        var result = Join.hashJoin(left, right, hashFcn, rightJoin);
-        expect(result).toEqual(permutationOf(rightExpected));
-      });
-
-      it('should do right join with a string', function() {
-        var result = Join.hashJoin(left, right, 'x', rightJoin);
-        expect(result).toEqual(permutationOf(rightExpected));
-      });
-
-      it('should do full join with a function', function() {
-        var result = Join.hashJoin(left, right, hashFcn, fullJoin);
-        expect(result).toEqual(permutationOf(fullExpected));
-      });
-
-      it('should do full join with a string', function() {
-        var result = Join.hashJoin(left, right, 'x', fullJoin);
-        expect(result).toEqual(permutationOf(fullExpected));
-      });
-
-      it('should do cross join with a function');
-      it('should do cross join with a string');
+      it('should do cross join');
     });
 
-    describe('using the fluent method', function() {
-      it('should do inner join with a function', function() {
-        var result = Join
-          .selectFrom(left)
-          .hashJoin(right, hashFcn, innerJoin)
-          .execute();
-        expect(result).toEqual(permutationOf(innerExpected));
+    describe('called fluently', function() {
+      tests.forEach(function(t) {
+        describe('should do ' + t.name + '-join', function() {
+          it('with a function', function() {
+            var result = Join
+              .selectFrom(t.left)
+              .hashJoin(t.right, t.hash, t.join)
+              .execute();
+            expect(result).toEqual(permutationOf(t.expected));
+          });
+
+          it('with a string', function() {
+            var result = Join
+              .selectFrom(t.left)
+              .hashJoin(t.right, t.field, t.join)
+              .execute();
+            expect(result).toEqual(permutationOf(t.expected));
+          });
+
+          it('with an array', function() {
+            var result = Join
+              .selectFrom(t.left)
+              .hashJoin(t.right, [t.field], t.join)
+              .execute();
+            expect(result).toEqual(permutationOf(t.expected));
+          });
+        });
       });
 
-      it('should do inner join with a string', function() {
-        var result = Join
-          .selectFrom(left)
-          .hashJoin(right, 'x', innerJoin)
-          .execute();
-        expect(result).toEqual(permutationOf(innerExpected));
-      });
-
-      it('should do left join with a function', function() {
-        var result = Join
-          .selectFrom(left)
-          .hashJoin(right, hashFcn, leftJoin)
-          .execute();
-        expect(result).toEqual(permutationOf(leftExpected));
-      });
-
-      it('should do left join with a string', function() {
-        var result = Join
-          .selectFrom(left)
-          .hashJoin(right, 'x', leftJoin)
-          .execute();
-        expect(result).toEqual(permutationOf(leftExpected));
-      });
-
-      it('should do right join with a function', function() {
-        var result = Join
-          .selectFrom(left)
-          .hashJoin(right, hashFcn, rightJoin)
-          .execute();
-        expect(result).toEqual(permutationOf(rightExpected));
-      });
-
-      it('should do right join with a string', function() {
-        var result = Join
-          .selectFrom(left)
-          .hashJoin(right, 'x', rightJoin)
-          .execute();
-        expect(result).toEqual(permutationOf(rightExpected));
-      });
-
-      it('should do full join with a function', function() {
-        var result = Join
-          .selectFrom(left)
-          .hashJoin(right, hashFcn, fullJoin)
-          .execute();
-        expect(result).toEqual(permutationOf(fullExpected));
-      });
-
-      it('should do full join with a string', function() {
-        var result = Join
-          .selectFrom(left)
-          .hashJoin(right, 'x', fullJoin)
-          .execute();
-        expect(result).toEqual(permutationOf(fullExpected));
-      });
-
-      it('should do cross join with a function');
-      it('should do cross join with a string');
+      it('should do cross join');
     });
 
   });
 
-  describe('using a comparator function', function() {
-    function comparator(e1, e2) {
-      return e1.x - e2.x;
-    }
+  describe('using a merge-join algorithm', function() {
+    describe('called statically', function() {
+      tests.forEach(function(t) {
+        describe('should do ' + t.name + '-join', function() {
+          it('with a function', function() {
+            var result = Join.mergeJoin(t.left, t.right, t.comparator, t.join);
+            expect(result).toEqual(t.expected);
+          });
 
-    describe('using the static method', function() {
-      it('should do inner join with a function', function() {
-        var result = Join.mergeJoin(left, right, comparator, innerJoin);
-        expect(result).toEqual(innerExpected);
+          it('with a string', function() {
+            var result = Join.mergeJoin(t.left, t.right, t.field, t.join);
+            expect(result).toEqual(t.expected);
+          });
+
+          it('with an array', function() {
+            var result = Join.mergeJoin(t.left, t.right, [t.field], t.join);
+            expect(result).toEqual(t.expected);
+          });
+        });
       });
 
-      it('should do inner join with a string', function() {
-        var result = Join.mergeJoin(left, right, 'x', innerJoin);
-        expect(result).toEqual(innerExpected);
-      });
-
-      it('should do left join with a function', function() {
-        var result = Join.mergeJoin(left, right, comparator, leftJoin);
-        expect(result).toEqual(leftExpected);
-      });
-
-      it('should do left join with a string', function() {
-        var result = Join.mergeJoin(left, right, 'x', leftJoin);
-        expect(result).toEqual(leftExpected);
-      });
-
-      it('should do right join with a function', function() {
-        var result = Join.mergeJoin(left, right, comparator, rightJoin);
-        expect(result).toEqual(rightExpected);
-      });
-
-      it('should do right join with a string', function() {
-        var result = Join.mergeJoin(left, right, 'x', rightJoin);
-        expect(result).toEqual(rightExpected);
-      });
-
-      it('should do full join with a function', function() {
-        var result = Join.mergeJoin(left, right, comparator, fullJoin);
-        expect(result).toEqual(fullExpected);
-      });
-
-      it('should do full join with a string', function() {
-        var result = Join.mergeJoin(left, right, 'x', fullJoin);
-        expect(result).toEqual(fullExpected);
-      });
-
-      it('should do cross join with a function');
-      it('should do cross join with a string');
+      it('should do cross join');
     });
 
-    describe('using the fluent method', function() {
-      it('should do inner join with a function', function() {
-        var result = Join
-          .selectFrom(left)
-          .mergeJoin(right, comparator, innerJoin)
-          .execute();
-        expect(result).toEqual(innerExpected);
+    describe('called fluently', function() {
+      tests.forEach(function(t) {
+        describe('should do ' + t.name + '-join', function() {
+          it('with a function', function() {
+            var result = Join
+              .selectFrom(t.left)
+              .mergeJoin(t.right, t.comparator, t.join)
+              .execute();
+            expect(result).toEqual(t.expected);
+          });
+
+          it('with a string', function() {
+            var result = Join
+              .selectFrom(t.left)
+              .mergeJoin(t.right, t.field, t.join)
+              .execute();
+            expect(result).toEqual(t.expected);
+          });
+
+          it('with an array', function() {
+            var result = Join
+              .selectFrom(t.left)
+              .mergeJoin(t.right, [t.field], t.join)
+              .execute();
+            expect(result).toEqual(t.expected);
+          });
+        });
       });
 
-      it('should do inner join with a string', function() {
-        var result = Join
-          .selectFrom(left)
-          .mergeJoin(right, 'x', innerJoin)
-          .execute();
-        expect(result).toEqual(innerExpected);
-      });
-
-      it('should do left join with a function', function() {
-        var result = Join
-          .selectFrom(left)
-          .mergeJoin(right, comparator, leftJoin)
-          .execute();
-        expect(result).toEqual(leftExpected);
-      });
-
-      it('should do left join with a string', function() {
-        var result = Join
-          .selectFrom(left)
-          .mergeJoin(right, 'x', leftJoin)
-          .execute();
-        expect(result).toEqual(leftExpected);
-      });
-
-      it('should do right join with a function', function() {
-        var result = Join
-          .selectFrom(left)
-          .mergeJoin(right, comparator, rightJoin)
-          .execute();
-        expect(result).toEqual(rightExpected);
-      });
-
-      it('should do right join with a string', function() {
-        var result = Join
-          .selectFrom(left)
-          .mergeJoin(right, 'x', rightJoin)
-          .execute();
-        expect(result).toEqual(rightExpected);
-      });
-
-      it('should do full join with a function', function() {
-        var result = Join
-          .selectFrom(left)
-          .mergeJoin(right, comparator, fullJoin)
-          .execute();
-        expect(result).toEqual(fullExpected);
-      });
-
-      it('should do full join with a string', function() {
-        var result = Join
-          .selectFrom(left)
-          .mergeJoin(right, 'x', fullJoin)
-          .execute();
-        expect(result).toEqual(fullExpected);
-      });
-
-      it('should do cross join with a function');
-      it('should do cross join with a string');
+      it('should do cross join');
     });
 
   });
+
 });
