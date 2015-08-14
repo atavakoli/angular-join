175cab23d17956477f27ed1211811432afb45447
Ali Tavakoli <ali.tavakoli@gmail.com>
2015-08-14 14:34:20 -0400

    Added karma coverage report to testing
    The default html-based code coverage report is now generated using
    karma-coverage.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,13 +25,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'angular-join.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'html'],
+    reporters: ['progress', 'html', 'coverage'],
 
 
     // web server port
